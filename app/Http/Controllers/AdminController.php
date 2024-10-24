1d7e1d9f58d8c281f16b8faedd4a014848da2317
winshuka <vinshukakannangara@gmail.com>
2024-10-24 18:24:56 +0530

    fetch questions with answers and show them all in the dashboard view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -11,8 +12,7 @@ class AdminController extends Controller
 {
     public function showDashboard(): View|Factory|Application
     {
-
-
-        return view('admin.dashboard');
+        $questions = Question::with('answers')->get();
+        return view('admin.dashboard')->with(['questions' => $questions]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,16 +36,19 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                @foreach($questions as $question)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$question->question}}
+                        </th>
+                        @foreach($question->answers as $answer)
+                            <td class="px-6 py-4">
+                                {{$answer->answer }}
+                            </td>
+                        @endforeach
 
-                    </th>
-                    <td class="px-6 py-4">
-
-                    </td>
-
-                </tr>
-
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
         </div>
